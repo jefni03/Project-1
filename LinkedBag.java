d1512b0ce71d445b22887d2293b71d64b14bc806
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2022-09-22 00:09:39 -0700

    9/22 Finished Coding

--- LinkedBag.java
+++ LinkedBag.java
@@ -27,37 +27,95 @@ public class LinkedBag<T> implements BagInterface<T>
 
     public T remove()
     {
-
+        T result = null;
+        if(firstNode != null)
+        {
+            result = firstNode.getData();
+            firstNode = firstNode.getNextNode();
+            numberOfEntries--;
+        }
+        return result;
     }
 
     public boolean remove(T entry)
     {
-
+         boolean result = false;
+         Node nodeN = getReferenceTo(entry);
+         if (nodeN != null)
+        {
+            nodeN.setData(firstNode.getData()); 
+            firstNode = firstNode.getNextNode(); 
+            numberOfEntries--;
+         
+         result = true;
+        } 
+    
+      return result;
     }
 
     public void clear()
     {
-
+        while(!isEmpty())
+        {
+            remove();
+        }
     }
 
     public int getFrequencyOf(T entry)
     {
-
+        int frequency = 0;
+        int count = 0;
+        Node currentNode = firstNode;
+        while((count < numberOfEntries) && (currentNode != null))
+        {
+            if(entry.equals(currentNode.getData()))
+            {
+                frequency++;
+            }
+            count++;
+            currentNode = currentNode.getNextNode();
+        }
+        return frequency;
     }
 
     public boolean contains(T entry)
     {
-
+        boolean found = false;
+        Node currentNode = firstNode;
+        while(!found && (currentNode != null))
+        {
+            if(entry.equals(currentNode.getData()))
+            {
+                found = true;
+            }
+            else
+            {
+                currentNode = currentNode.getNextNode();
+            }
+        }
+        return found;
     }
 
     public T[] toArray()
     {
+        @SuppressWarnings("unchecked") //need or else java compiler will spit out warning
+        T[] array = (T[]) new Object[numberOfEntries]; //makes an array of objects but cast it to be generic array 
 
+        int i=0;
+        Node currentNode=firstNode;
+        while((i<numberOfEntries)&&(currentNode!=null)){ 
+            array[i]=currentNode.getData();
+            i++;
+            currentNode=currentNode.getNextNode();
+        }
+        return array;
     }
 
     public T union(T bag)
     {
-        
+        //clone bags before looking through them so it isn't possible to change data inside of original
+        //
+
     }
 
     public T intersection(T bag)
@@ -75,23 +133,34 @@ public class LinkedBag<T> implements BagInterface<T>
         private T data;
         private Node nextNode;
 
+        //Node constructor, which has data and connection to next node
         private Node(T data, Node nextNode){
             this.data=data;
             this.nextNode=nextNode;    
         }
+        
+        //Node constructor, which makes node without a connection to another node
+        private Node(T data){
+            this.data=data;
+            nextNode=null;
+        }
 
+        //returns the data of current node
         private T getData(){
             return data;
         }
 
+        //sets data of current node
         private void setData(T data){
             this.data=data;
         }
         
+        //returns the node after current node
         private Node getNextNode(){
             return nextNode;
         }
 
+        //sets the next node after the current node to something else
         private void setNextNode(Node nextNode){
             this.nextNode=nextNode;
         }
--- ResizeableArrayBag.java
+++ ResizeableArrayBag.java
@@ -3,6 +3,7 @@ public class ResizeableArrayBag<T> implements BagInterface<T>
     private final T[] bag;
     private static final int defaultCapacity = 25;
     private int numberOfEntries;
+    private boolean integrityOK;
 
     public void ArrayBag()
     {
@@ -10,6 +11,96 @@ public class ResizeableArrayBag<T> implements BagInterface<T>
     }
 
     public void ArrayBag(int Capacity)
+    {
+
+    }
+
+    private void checkIntegrity()
+    {
+        if(!integrityOK)
+        {
+            throw new SecurityException("ArrayBag object is corrupt");
+        }
+    }
+
+    public int getCurrentSize()
+    {
+
+    }
+
+    public boolean isEmpty()
+    {
+
+    }
+
+    private void checkCapacity(int capacity)
+    {
+        if(capacity > MAX_CAPACITY)
+        {
+            throw new illegalStateException("Attempt to create a bag whose capacity exeeds allowed maximum of " + MAX_CAPACITY);
+        }
+    }
+
+    private void doubleCapacity()
+    {
+        int newLength = 2*bag.length;
+        checkCapacity(newLength);
+        bag = Arrays.copyOf(bag, newLength);
+    }
+
+    public boolean add(T newEntry)
+    {
+        checkIntegrity();
+        boolean result = true;
+        if(isArrayFull())
+        {
+            doubleCapacity();
+        }
+        bag[numberOfEntries] = newEntry;
+        numberOfEntries++;
+    }
+
+    public T remove()
+    {
+
+    }
+
+    public boolean remove(T entry)
+    {
+
+    }
+
+    public void clear()
+    {
+
+    }
+
+    public int getFrequencyOf(T entry)
+    {
+
+    }
+
+    public boolean contains(T entry)
+    {
+
+    }
+
+    public T[] toArray()
+    {
+
+    }
+
+    public T union(T bag)
+    {
+
+    }
+
+    public T intersection(T bag)
+    {
+
+    }
+
+    public T difference(T bag)
     {
 
     }
