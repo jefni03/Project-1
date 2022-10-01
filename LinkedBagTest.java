76cb64d437c43a895da837bfabc0153cf22c415c
Justin Nguyen <justinnguyen2245@gmail.com>
2022-09-30 20:36:40 -0700

    testing push

--- LinkedBagTest.java
+++ LinkedBagTest.java
@@ -1,3 +1,31 @@
+import org.junit.Test;
+
 public class LinkedBagTest {
     
+    //bag scenarios
+    //1. bag1 has multiple of same entry in a row
+    //2. bag2 has multiple of same entry in a row
+    //3. bag1 and bag2 have multiple of same entry in a row
+
+    //1. bag1 has multiple
+
+
+    @Test
+    public void shouldGiveUnionWhen(){
+        BagDriver<Integer> LinkedBag1 = new LinkedBag();
+        LinkedBag1.add(5);
+        LinkedBag1.add(4);
+        LinkedBag1.add(2);
+        LinkedBag1.add(5);
+    }
+
+    @Test
+    public void shouldGiveIntersect(){
+
+    }
+
+    @Test
+    public void shouldGiveDIfference(){
+
+    }
 }
--- ResizeableArrayBag.java
+++ ResizeableArrayBag.java
@@ -1,107 +1,107 @@
-public class ResizeableArrayBag<T> implements BagInterface<T>
-{
-    private final T[] bag;
-    private static final int defaultCapacity = 25;
-    private int numberOfEntries;
-    private boolean integrityOK;
+// public class ResizeableArrayBag<T> implements BagInterface<T>
+// {
+//     private final T[] bag;
+//     private static final int defaultCapacity = 25;
+//     private int numberOfEntries;
+//     private boolean integrityOK;
 
-    public void ArrayBag()
-    {
+//     public void ArrayBag()
+//     {
 
-    }
+//     }
 
-    public void ArrayBag(int Capacity)
-    {
+//     public void ArrayBag(int Capacity)
+//     {
 
-    }
+//     }
 
-    private void checkIntegrity()
-    {
-        if(!integrityOK)
-        {
-            throw new SecurityException("ArrayBag object is corrupt");
-        }
-    }
+//     private void checkIntegrity()
+//     {
+//         if(!integrityOK)
+//         {
+//             throw new SecurityException("ArrayBag object is corrupt");
+//         }
+//     }
 
-    public int getCurrentSize()
-    {
+//     public int getCurrentSize()
+//     {
 
-    }
+//     }
 
-    public boolean isEmpty()
-    {
+//     public boolean isEmpty()
+//     {
 
-    }
+//     }
 
-    private void checkCapacity(int capacity)
-    {
-        if(capacity > MAX_CAPACITY)
-        {
-            throw new illegalStateException("Attempt to create a bag whose capacity exeeds allowed maximum of " + MAX_CAPACITY);
-        }
-    }
+//     private void checkCapacity(int capacity)
+//     {
+//         if(capacity > MAX_CAPACITY)
+//         {
+//             throw new illegalStateException("Attempt to create a bag whose capacity exeeds allowed maximum of " + MAX_CAPACITY);
+//         }
+//     }
 
-    private void doubleCapacity()
-    {
-        int newLength = 2*bag.length;
-        checkCapacity(newLength);
-        bag = Arrays.copyOf(bag, newLength);
-    }
+//     private void doubleCapacity()
+//     {
+//         int newLength = 2*bag.length;
+//         checkCapacity(newLength);
+//         bag = Arrays.copyOf(bag, newLength);
+//     }
 
-    public boolean add(T newEntry)
-    {
-        checkIntegrity();
-        boolean result = true;
-        if(isArrayFull())
-        {
-            doubleCapacity();
-        }
-        bag[numberOfEntries] = newEntry;
-        numberOfEntries++;
-    }
+//     public boolean add(T newEntry)
+//     {
+//         checkIntegrity();
+//         boolean result = true;
+//         if(isArrayFull())
+//         {
+//             doubleCapacity();
+//         }
+//         bag[numberOfEntries] = newEntry;
+//         numberOfEntries++;
+//     }
 
-    public T remove()
-    {
+//     public T remove()
+//     {
 
-    }
+//     }
 
-    public boolean remove(T entry)
-    {
+//     public boolean remove(T entry)
+//     {
 
-    }
+//     }
 
-    public void clear()
-    {
+//     public void clear()
+//     {
 
-    }
+//     }
 
-    public int getFrequencyOf(T entry)
-    {
+//     public int getFrequencyOf(T entry)
+//     {
 
-    }
+//     }
 
-    public boolean contains(T entry)
-    {
+//     public boolean contains(T entry)
+//     {
 
-    }
+//     }
 
-    public T[] toArray()
-    {
+//     public T[] toArray()
+//     {
 
-    }
+//     }
 
-    public BagInterface<T> union(BagInterface<T> bag)
-    {
+//     public BagInterface<T> union(BagInterface<T> bag)
+//     {
 
-    }
+//     }
 
-    public BagInterface<T> intersection(BagInterface<T> bag)
-    {
+//     public BagInterface<T> intersection(BagInterface<T> bag)
+//     {
 
-    }
+//     }
 
-    public BagInterface<T> difference(BagInterface<T> bag)
-    {
+//     public BagInterface<T> difference(BagInterface<T> bag)
+//     {
 
-    }
-}
+//     }
+// }
