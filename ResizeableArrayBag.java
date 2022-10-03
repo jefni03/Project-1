21f7b6d5dda5fd262601d662bd537d16bce6f108
Jeffrey <nijeffrey2003@gmail.com>
2022-10-02 20:32:35 -0700

    add

--- ResizeableArrayBag.java
+++ ResizeableArrayBag.java
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-// public class ResizeableArrayBag<T> implements BagInterface<T>
-// {
-//     private final T[] bag;
-//     private static final int defaultCapacity = 25;
-//     private int numberOfEntries;
-//     private boolean integrityOK;
-
-//     public void ArrayBag()
-//     {
-=======
 import java.util.Arrays;
 
 public class ResizeableArrayBag<T> implements BagInterface<T>
@@ -21,91 +10,9 @@ public class ResizeableArrayBag<T> implements BagInterface<T>
 
     public void ResizeableArrayBag()
     {
->>>>>>> 2a2768ddd41459b26ad9b999edbbf0665d961c9d
 
 //     }
 
-<<<<<<< HEAD
-//     public void ArrayBag(int Capacity)
-//     {
-
-//     }
-
-//     private void checkIntegrity()
-//     {
-//         if(!integrityOK)
-//         {
-//             throw new SecurityException("ArrayBag object is corrupt");
-//         }
-//     }
-
-//     public int getCurrentSize()
-//     {
-
-//     }
-
-//     public boolean isEmpty()
-//     {
-
-//     }
-
-//     private void checkCapacity(int capacity)
-//     {
-//         if(capacity > MAX_CAPACITY)
-//         {
-//             throw new illegalStateException("Attempt to create a bag whose capacity exeeds allowed maximum of " + MAX_CAPACITY);
-//         }
-//     }
-
-//     private void doubleCapacity()
-//     {
-//         int newLength = 2*bag.length;
-//         checkCapacity(newLength);
-//         bag = Arrays.copyOf(bag, newLength);
-//     }
-
-//     public boolean add(T newEntry)
-//     {
-//         checkIntegrity();
-//         boolean result = true;
-//         if(isArrayFull())
-//         {
-//             doubleCapacity();
-//         }
-//         bag[numberOfEntries] = newEntry;
-//         numberOfEntries++;
-//     }
-
-//     public T remove()
-//     {
-
-//     }
-
-//     public boolean remove(T entry)
-//     {
-
-//     }
-
-//     public void clear()
-//     {
-
-//     }
-
-//     public int getFrequencyOf(T entry)
-//     {
-
-//     }
-
-//     public boolean contains(T entry)
-//     {
-
-//     }
-
-//     public T[] toArray()
-//     {
-
-//     }
-=======
     public void ResizeableArrayBag(int Capacity)
     {
         
@@ -247,7 +154,6 @@ public class ResizeableArrayBag<T> implements BagInterface<T>
         }
         return result;
     }
->>>>>>> 2a2768ddd41459b26ad9b999edbbf0665d961c9d
 
 //     public BagInterface<T> union(BagInterface<T> bag)
 //     {
