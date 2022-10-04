4a5af4250bdf53b7e3c5107f7a319a0fab68623b
Jeffrey <nijeffrey2003@gmail.com>
2022-10-03 18:56:33 -0700

    comments for driver

--- BagDriver.java
+++ BagDriver.java
@@ -1,11 +1,20 @@
+/**
+* Import arrays framework 
+*/
 import java.util.Arrays;
 public class BagDriver 
 {
     public static void main(String args[])
     {
+    /**
+     * Creaates two bags of LinkedBag Type that takes String values
+     */
     BagInterface <String> bag1 = new LinkedBag <String>();
     BagInterface <String> bag2 = new LinkedBag <String>();
 
+    /**
+     * Bags are filled with letters to test the methods 
+     */
     bag1.add("a");
     bag1.add("b");
     bag1.add("c");
@@ -14,15 +23,23 @@ public class BagDriver
     bag2.add("d");
     bag2.add("e");
 
+    /**
+     * Methods are called with the results printed out 
+     */
     System.out.println(Arrays.toString(bag1.union(bag2).toArray()));
     System.out.println(Arrays.toString(bag1.intersection(bag2).toArray()));
     System.out.println(Arrays.toString(bag1.difference(bag2).toArray()));
     System.out.println(Arrays.toString(bag2.difference(bag1).toArray()));
 
-    
+    /**
+     * Creaates two bags of ResizeableArrayBag Type that takes String values
+     */
     BagInterface <String> bag3 = new ResizeableArrayBag <String>();
     BagInterface <String> bag4 = new ResizeableArrayBag <String>();
 
+    /**
+     * Bags are filled with letters to test the methods 
+     */
     bag3.add("a");
     bag3.add("b");
     bag3.add("c");
@@ -31,6 +48,9 @@ public class BagDriver
     bag4.add("d");
     bag4.add("e");
 
+    /**
+     * Methods are called with the results printed out 
+     */
     System.out.println(Arrays.toString(bag3.union(bag4).toArray()));
     System.out.println(Arrays.toString(bag3.intersection(bag4).toArray()));
     System.out.println(Arrays.toString(bag3.difference(bag4).toArray()));
